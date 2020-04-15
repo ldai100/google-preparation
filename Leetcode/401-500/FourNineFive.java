a3df77f16034e5b166fca0b945a4c17b0306e471
lidai <lidaivet@gmail.com>
2020-04-14 23:06:13 -0400

    added new solution to leetcode 459

--- Leetcode/401-500/FourNineFive.java
+++ Leetcode/401-500/FourNineFive.java
@@ -12,7 +12,22 @@ public class FourNineFive {
                 return true;
             }
         }
+        return false;
+    }
 
+    // second version after using modulo and string builder. speed went up 90%;
+    public boolean repeatedSubstringPattern2(String s) {
+        for(int i = 1; i < s.length()/2 + 1; i++) {
+            String sub = s.substring(0, i);
+            StringBuilder builder = new StringBuilder();
+            builder.append(sub);
+            while(s.length() % i == 0 && builder.length() < s.length()) {
+                builder.append(sub);
+            }
+            if(builder.toString().equals(s)) {
+                return true;
+            }
+        }
         return false;
     }
 }
