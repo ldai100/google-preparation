cb56aec90689021268e4746e21354054c7a1c418
lidai <lidaivet@gmail.com>
2020-05-02 00:50:26 -0400

    added new solution to leetcode 338

--- Leetcode/301-400/ThreeThreeEight.java
+++ Leetcode/301-400/ThreeThreeEight.java
@@ -14,4 +14,13 @@ public class ThreeThreeEight {
         }
         return ans;
     }
+
+    // using dynamic programming;
+    public int[] countBits2(int num) {
+        int[] ans = new int[num+1];
+        for(int i = 1; i<= num; i++) {
+            ans[i] = ans[i & (i-1)] + 1;
+        }
+        return ans;
+    }
 }
